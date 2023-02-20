344d0a9d1794d0dbab59a9fd8ec2dedd65e29703
abaez8031 <abaez8031@gmail.com>
2023-02-19 22:51:53 -0500

    finished

--- W6D5/UserAuth/app/models/user.rb
+++ W6D5/UserAuth/app/models/user.rb
@@ -4,23 +4,41 @@ class User < ApplicationRecord
   validates :password, length: {minimum: 6, allow_nil: true}
   before_validation :ensure_session_token
 
+  attr_reader :password
+
   def self.find_by_credentials(username,password)
-    
+    user = User.find_by(username: username)
+    if user && user.is_password?(password)
+      user
+    else
+      nil
+    end
   end
 
   def generate_unique_session_token
+    loop do
+      session_token = SecureRandom::urlsafe_base64(16)
+      return session_token unless User.exists?(session_token: session_token)
+    end
   end
 
-  def password=
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
   end
 
   def is_password?
+    BCrypt::Password.new(password_digest).is_password?(password)
   end
 
   def reset_session_token!
+    self.session_token = generate_unique_session_token
+    self.save!
+    self.session_token
   end
 
   def ensure_session_token
+    self.session_token ||= generate_unique_session_token
   end
 
 end
